ea9fad7e18299ecf50ea8a2df304508508c7d735
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-18 17:28:55 -0300

    fix: add getters in UserView

--- src/main/java/br/com/caelum/clines/api/users/UserView.java
+++ src/main/java/br/com/caelum/clines/api/users/UserView.java
@@ -1,8 +1,10 @@
 package br.com.caelum.clines.api.users;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class UserView {
     private Long id;
     private String name;
